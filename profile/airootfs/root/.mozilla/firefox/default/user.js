73e1b3acc5e0144f79855989c9346b4e73f449ee
SavinMax <savinmax2.ru@gmail.com>
2026-08-14 13:41:59 +0300

    build: setup custom configs, extensions and firefox policies

---
+++ profile/airootfs/root/.mozilla/firefox/default/user.js
@@ -0,0 +1,8 @@
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("browser.theme.content-theme", 0);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
